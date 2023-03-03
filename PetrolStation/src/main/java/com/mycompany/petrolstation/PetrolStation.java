3836e8b57f3ca1e3dc67b614bd0ae230f53db28f
dfeiAlex <20221937@dfei.ie>
2023-03-03 11:10:27 +0000

    Initial Commit

---
+++ PetrolStation/src/main/java/com/mycompany/petrolstation/PetrolStation.java
@@ -0,0 +1,9 @@
+package main.java.com.mycompany.petrolstation;
+
+/** | @author 20221937 | **/
+
+public class PetrolStation {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ PetrolStation/src/main/java/com/mycompany/petrolstation/TestPetrolStation.java
@@ -0,0 +1,7 @@
+package main.java.com.mycompany.petrolstation;
+
+/** | @author 20221937 | **/
+
+public class TestPetrolStation {
+    
+}
